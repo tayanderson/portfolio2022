593e629102aa0ba03a222d20ea6c7b6743bb69f9
Taylor Anderson <tanderson018@gmail.com>
2023-09-12 09:54:46 -0500

    ga config

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,8 +52,12 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
+          "6095311407",
           "G-N8BPYJJZG6", // Google Analytics / GA
         ],
+        pluginConfig: {
+          head: true
+        }
       },
     },
   ],
